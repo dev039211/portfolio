9b1d4ef2d0b5bdd2dee6c059a56fcde5d25cc46a
dev039211 <108935683+dev039211@users.noreply.github.com>
2022-07-11 10:52:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+function initMap() {
+  map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
